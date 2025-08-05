53c04b7d6450fa25dee6e7722faf94881d5bca92
matth <matthewiva13@gmail.com>
2025-08-05 16:05:20 +0300

    build

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default defineConfig({
+  input: "index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    terser(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+  ],
+  external: [],
+});
